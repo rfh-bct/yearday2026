26bcfac7232623d2fd78437caa1bf7ceacce086e
rfh-bct <rfh@blackcapitaltechnology.com>
2026-02-09 20:31:47 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useParams } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, useParams } from 'react-router-dom';
 import Page from './Page';
 import { pageContents } from './pageContent';
 
