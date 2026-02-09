0d49ff79c07a32bb51df0b302ec313089bf30ebd
rfh-bct <rfh@blackcapitaltechnology.com>
2026-02-09 20:29:50 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Required for GitHub Pages if the site is at https://<user>.github.io/<repo>/
+  // Use '/' if you use a custom domain or GitHub user site (e.g. username.github.io).
+  base: '/unlock-app/',
 })
